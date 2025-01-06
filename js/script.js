f5beb7a7a5fe5d445802edcbfb90fddd664bae50
DaniloS-Lima <danillolima2022@yahoo.com>
2025-01-06 13:38:04 -0300

    second commit

---
+++ js/script.js
@@ -0,0 +1,164 @@
+const items = {
+    item1: {
+      title: "BENEFÍCIOS DE NÍVEL",
+      description: `
+        <strong>ITEM - Caixa de Nível</strong><br>
+        - Pode dropar um dos seguintes itens:<br>
+        1 Refin Amber<br>
+        1 Refin Citrine<br>
+        1 Refin Beryl<br>
+        1 Refin Shappire<br><br>
+        <strong>ITEM - Super Caixa de Nível</strong><br>
+        - Pode dropar um dos seguintes itens:<br>
+        1 Super Amber<br>
+        1 Super Citrine<br>
+        1 Super Beryl<br>
+        1 Super Shappire<br><br>
+        <strong>PREMIAÇÕES</strong><br>
+        - Level 10:<br>
+        Premiação: 3xExp + 1 Caixa de Nível + 1000 PPS<br><br>
+        - Level 20:<br>
+        Premiação: 3xExp + Caixa de Equipamentos + 4.000 PPS<br><br>
+        - Level 50:<br>
+        Premiação: 3xExp + 2 Caixas de Nível + 5.000 PPS<br><br>
+        - Level 100:<br>
+        Premiação: 3xExp + 3 Caixas de Nível + 10.000 PPS<br><br>
+        - Level 150:<br>
+        Premiação: 3xExp + 5 Caixas de Nível + 10.000 PPS<br><br>
+        - Level 200:<br>
+        Premiação: 3xExp + 5 Caixas de Nível + 20.000 PPS<br><br>
+        - Level 250:<br>
+        Premiação: 1 Super Caixa de Nível + 20.000 PPS<br><br>
+        - Level 255:<br>
+        Premiação: 1 Super Caixa de Nível + 30.000 PPS
+      `
+    },
+    item2: {
+      title: "PREMIO DO REI",
+      description: `
+        <strong>CRISTAL MÁGICO - PARA QUE SERVE?</strong><br>
+        - Item utilizado para conseguir uma MoonBox ou Super MoonBox.<br><br>
+        <strong>PREMIAÇÃO</strong><br>
+        - 5 Cristais Mágicos + 5.000 EPS (Em cada Baú).<br><br>
+        <strong>HORÁRIO</strong><br>
+        - AINDA NÃO DEFINIDO.<br><br>
+        <strong>COMO FUNCIONA?</strong><br>
+        O rei irá espalhar 2 baús por mapa, sendo eles Elvin / Gobi / Pantano / Ilha.<br>
+        Terá 2 localizações aleatórias por cada mapa!
+      `
+    },
+    item3: {
+      title: "ARMAS",
+      description: `
+        <strong>ADICIONADO</strong><br>
+        - Cartão de Bronze<br>
+        - Cartão de Prata<br>
+        - Cartão de Ouro<br>
+        - Cartão da Espada<br>
+        - Cartão do Cajado<br>
+        - Cartão do Cetro<br><br>
+        <strong>COMO DROPAR?</strong><br>
+        - Após chegar no nível 255, você tem a chance de dropar o cartão de bronze em CataMaze!<br><br>
+        <strong>PARA QUE SERVE E COMO FUNCIONA?</strong><br>
+        - Você precisa de 15 Cartões de bronze para trocar por 1 Cartão de prata e 15 Cartões de prata para trocar por 1 Cartão de ouro.<br>
+        - Você vai precisar de 5 Cartões de ouro para trocar por 1 Cartão da Espada, 1 Cartão do Cajado ou 1 Cartão do Cetro.<br>
+        - Os cartões de Espada / Cajado / Cetro vai ser utilizado para atualizar sua arma.<br><br>
+        <strong>TROCAS</strong><br>
+        - Na primeira trocar você vai precisar de uma Espada / Cajado / Cetro nível 125 para trocar por outra arma nível 137.<br>
+        - 137 >> 150<br>
+        - 150 >>> 255<br>
+        - Todas as trocas você vai precisar de 1 Cartão apenas.
+      `
+    },
+    item4: {
+      title: "REBORNE E PETS",
+      description: `
+        Olá, venho informar a respeito dos rebornes!<br><br>
+        Todos os pets irão subir o HP ao rebornar, para ter uma maior diversidade de pets sendo feito no servidor, assim não ficando apenas Dodo e Lulu .<br><br>
+        Continuando sobre os pets, no shopping vai ter uma grande diversidade de pets, incluindo os pets astrais.<br><br>
+        Os pontos de rebornes vai ser de 5 a 10 pontos por reborne!
+      `
+    },
+    item5: {
+      title: "DROP DE BOSS",
+      description: `
+        <span class="boss-refin">BOSS REFIN</span><br>
+        - <strong>Drop Padrão:</strong><br>
+        2 - 4 Yellow<br>
+        2 - 3 Violet<br>
+        1 - 2 Card 30 EP's<br><br>
+        - <strong>Drop especial:</strong><br>
+        Após 15 Boss, 10% de chance de dropar 1 Pack de 5 Bolls.<br>
+        Após matar 10 Boss, drop de 1 Cristal Mágico.<br><br>
+        <span class="boss-unico">BOSS ÚNICO</span><br>
+        - <strong>Drop Padrão:</strong><br>
+        3 - 5 Yellow<br>
+        3 - 4 Violet<br>
+        0 - 1 Red<br>
+        1 - 2 Card 50 EP's<br><br>
+        - <strong>Drop especial:</strong><br>
+        Após 10 Boss, 10% de chance de dropar 1 Pack de 5 Bolls.<br>
+        Após matar 5 Boss, drop de 1 Cristal Mágico.<br><br>
+        <span class="boss-elite">BOSS ELITE</span><br>
+        - <strong>Drop Padrão:</strong><br>
+        4 - 5 Yellow<br>
+        3 - 5 Violet<br>
+        2 - 3 Red<br>
+        1 - 2 Card 80 EP's<br><br>
+        - <strong>Drop especial:</strong><br>
+        Após 5 Boss, 10% de chance de dropar 1 Pack de 5 Bolls.<br>
+        Após matar 3 Boss, drop de 1 Cristal Mágico.<br><br>
+        <strong>CRISTAL MÁGICO</strong><br>
+        Item utilizado para pegar MoonBox e SuperMoonBox:<br>
+        - 30 Cristais = 1 MoonBox<br>
+        - 40 Cristais = 1 SuperMoonBox
+      `
+    },
+    item6: {
+      title: "QUEST DE INVOCAR 3 PETS",
+      description: `
+        Refiz a quest para que você que saia da mesma quest de sempre.<br><br>
+        <strong>MISSÕES</strong><br>
+        São 20 missões no total, que são:<br><br>
+        - Você precisa matar 100 Gnomos, em MisteryMash (ElvinCity).<br><br>
+        - Você precisa matar 20 Boss (Refin/Unico/Elite).<br><br>
+        - Você precisa matar 500 Demônio do Deserto (Gobi Desert).<br><br>
+        - Você precisa matar 30 Boss (Refin/Unico/Elite).<br><br>
+        - Você precisa matar 500 Troglodita (Pantano).<br><br>
+        - Você precisa matar 50 Boss (Refin/Unico/Elite).<br><br>
+        - Você precisa matar 2000 mobs em CataMaze.<br><br>
+        - Você precisa que converse com o NPC em Cronus.<br><br>
+        - Você precisa que converse com o NPC em Gobi.<br><br>
+        - Você precisa que converse com o NPC em Ilha.<br><br>
+        - Você precisa que me entregue o pet Mago ou Guerreiro 15*.<br><br>
+        - Você precisa que me entregue o pet Mago ou Guerreiro 20*.<br><br>
+        - Você precisa que converse com o NPC em Volcano.<br><br>
+        - Você precisa que me entregue 20 Super Pedras Amarelas.<br><br>
+        - Você precisa que me entregue 20 Super Pedras Violetas.<br><br>
+        - Você precisa que me entregue 20 Super Pedras Vermelhas.<br><br>
+        - Você precisa que converse com o NPC em LostLand.<br><br>
+        - Você precisa que mate mobs em IceMazee me traga um item.<br><br>
+        - Você precisa que me entregue 50.000 EP's.<br><br>
+        - Você precisa que me entregue 50.000 EP's.
+      `
+    }
+  };
+
+  // Selecionar elementos do DOM
+  const itemList = document.getElementById("itemList");
+  const infoDisplay = document.getElementById("infoDisplay");
+  
+  // Adicionar evento de mudança ao select
+  itemList.addEventListener("change", function () {
+    const selectedItem = itemList.value;
+
+    if (selectedItem) {
+      const item = items[selectedItem];
+      infoDisplay.innerHTML = `
+        <h2>${item.title}</h2>
+        <p>${item.description}</p>
+      `;
+    } else {
+      infoDisplay.innerHTML = `<p>Selecione um item acima para ver as informações.</p>`;
+    }
+  });
